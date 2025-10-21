af13284d4b83890659dd8116cb9aa6e7fd5e42de
KMINALI2005 <162717637+KMINALI2005@users.noreply.github.com>
2025-10-21 21:36:41 +0300

    Specify NDK version in build.gradle.kts
    Set specific NDK version to 27.0.12077973.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.fatora_pro"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"  // ✅ إضافة NDK version المطلوب
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
